7e430b71dad2290727651ba7c6bbdbf7a779582d
AbhishekMishra19k <abhishekmishra.19k@gmail.com>
2026-08-29 23:51:46 +0530

    changes made to build_files.sh to include the --break-system-packages flag in the pip install command. This allows for the installation of packages even if they conflict with system packages, which can be useful in certain environments.

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
